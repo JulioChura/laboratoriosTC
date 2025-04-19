4a037393492f17a35abbb4f97ca202b3b96441ce
Julio Chura <jchuraaca@unsa.edu.pe>
2025-04-18 23:52:46 -0500

    se implementa la  concatenacion

--- lab01/concatenacion.py
+++ lab01/concatenacion.py
@@ -1,2 +1,9 @@
-def concatenacion(str1, str2):
-    pass
+def concatenacion(L1, L2):
+    resultado = []
+    for cadena1 in L1:
+        for cadena2 in L2:
+            resultado.append(cadena1 + cadena2)
+    return resultado
+
+def printArray(arr):
+    print("{ " + ", ".join(arr) + " }")
--- lab01/main.py
+++ lab01/main.py
@@ -1,5 +1,5 @@
 from union import union
-from concatenacion import concatenacion
+from concatenacion import concatenacion, printArray
 from kleene import clausula_kleene
 
 
@@ -9,4 +9,7 @@ lenguajeA = ["ab", "bc", "ce", "ea", "bd"]
 lenguajeB = ["ad", "cb", "da", "eb", "dc"]
 lenguajeC = ["ac", "be", "de", "cd", "ba"]
 
+
 # probar las funciones
+print("Concatenacion= ", end="")
+printArray(concatenacion(lenguajeA, lenguajeB))
