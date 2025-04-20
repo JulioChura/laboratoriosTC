09ca4de448725c5820ed071e107f5752021bb000
Marco Quispe Flores <mquispeflores@unsa.edu.pe>
2025-04-20 18:26:22 -0500

    Agregando la funcion union, une dos lenguajes

--- lab01/union.py
+++ lab01/union.py
@@ -1,2 +1,10 @@
-def union(str1, str2):
-    pass
+def union(len1, len2):
+    resultado = []
+    for cadena1 in len1:
+        resultado.append(cadena1)
+    for cadena2 in len2:
+        resultado.append(cadena2)
+    return resultado
+
+def printArray(arr):
+    print("{ " + ", ".join(arr) + " }")
