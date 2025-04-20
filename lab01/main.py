700d9a9914e5bc7e5efa09dcb791e2d0e8ddbb9c
Marco Quispe Flores <mquispeflores@unsa.edu.pe>
2025-04-20 18:49:42 -0500

    Probando union

--- lab01/main.py
+++ lab01/main.py
@@ -13,3 +13,6 @@ lenguajeC = ["ac", "be", "de", "cd", "ba"]
 # probar las funciones
 print("Concatenacion= ", end="")
 printArray(concatenacion(lenguajeA, lenguajeB))
+
+print("Union= ", end="")
+printArray(union(lenguajeA, lenguajeB))
